2f502ac679fc0036cd9e45599208b2729958bf99
Trevor Wiegand <trevorwiegand92@gmail.com>
2021-07-06 19:40:44 -0500

    updated js files

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+
+router.get("/", async (req, res) => {
+  try {
+    // Pass serialized data and session flag into template
+    res.render("homepage");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/login", (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect("/profile");
+    return;
+  }
+
+  res.render("login");
+});
+
+module.exports = router;
