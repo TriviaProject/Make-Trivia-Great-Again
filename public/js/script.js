41a5269c9f7b7c643db2b3adc2a9e35003228fb5
Nabiha Khan <nabiha@northwestern.edu>
2021-07-15 18:56:32 -0500

    cleaning code

--- public/js/script.js
+++ public/js/script.js
@@ -1,20 +1,3 @@
-// function rotateFunction() {
-//   var min = 1024;
-//   var max = 9999;
-//   var deg = Math.floor(Math.random() * (max - min)) + min;
-//   document.getElementById("box").style.transform = "rotate(" + deg + "deg)";
-// }
-
-// var element = document.getElementById("mainbox");
-// element.classList.remove("animate");
-
-// setTimeout(function () {
-//   element.classList.add("animate");
-// }, 5000);
-
-// document
-//   .querySelectorAll(".category")
-//   .addEventListener("click", () => console.log("This is too hard"));
 
 var padding = { top: 20, right: 40, bottom: 0, left: 0 },
   w = 500 - padding.left - padding.right,
@@ -26,8 +9,6 @@ var padding = { top: 20, right: 40, bottom: 0, left: 0 },
   oldpick = [],
   color = d3.scale.category20(); //category20c()
 //randomNumbers = getRandomNumbers();
-
-//http://osric.com/bingo-card-generator/?title=HTML+and+CSS+BINGO!&words=padding%2Cfont-family%2Ccolor%2Cfont-weight%2Cfont-size%2Cbackground-color%2Cnesting%2Cbottom%2Csans-serif%2Cperiod%2Cpound+sign%2C%EF%B9%A4body%EF%B9%A5%2C%EF%B9%A4ul%EF%B9%A5%2C%EF%B9%A4h1%EF%B9%A5%2Cmargin%2C%3C++%3E%2C{+}%2C%EF%B9%A4p%EF%B9%A5%2C%EF%B9%A4!DOCTYPE+html%EF%B9%A5%2C%EF%B9%A4head%EF%B9%A5%2Ccolon%2C%EF%B9%A4style%EF%B9%A5%2C.html%2CHTML%2CCSS%2CJavaScript%2Cborder&freespace=true&freespaceValue=Web+Design+Master&freespaceRandom=false&width=5&height=5&number=35#results
 
 var data = [
   {
@@ -467,11 +448,6 @@ function spin(d) {
                     },
                   });
 
-                  // if (response.ok) {
-                  //   document.location.replace("/scores");
-                  // } else {
-                  //   alert("Failed to create project");
-                  // }
                 } else {
                   alert("Wrong answer, try again! (-5 points!)");
                   const scoreObject = await fetch("/api/scores").then(
